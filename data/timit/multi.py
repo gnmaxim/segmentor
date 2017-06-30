894dcb50c3c1503acdaefec49d292290f133520c
Maxim Gaina <maxim@alfa4c.alfa>
2017-06-30 15:10:10 +0200

    datset unpacking ultimated

--- data/timit/multi.py
+++ data/timit/multi.py
@@ -1,3 +1,19 @@
+"""
+    Script that assigns label to each extracted frame vector
+
+    consider this before training:
+        1. enabled/disabled noHeader openSMILE configuration
+        2. enabled/disabled voiced cutoff configuration
+            2a. enabled/disabled voicing probability
+            NOTED that a lot of cuted off frames are actually vowels
+        3. erase h#
+        4. tollerance (0.25 or 0)
+
+    done:
+        - eliminate ffmpeg dependecy (BETTER ffmpeg than without)
+        - multiprocess labeling
+"""
+
 import configparser as cp
 import itertools as it
 import pandas as pd
@@ -13,8 +29,8 @@ from functools import partial
 """
     These global constants will not be modified
 """
-# Number of processes to create
-PROC = 4
+# Number of core to use by default
+PHYS_CORES = 4
 
 # Some file extensions used here
 WAV_EXT = ".WAV"
@@ -29,9 +45,7 @@ CSV_PATH = "csv/"
 VOWELS = "vowels.csv"
 
 CONFIG_PATH = "../../configs/features.conf"
-PART_PATH = "../../configs/partitioning.conf"
 OUTPUT = "../../models/"
-SETS = ["trainset", "validationset", "testset"]
 
 VOWEL = 1
 NONVOWEL = 0
@@ -43,9 +57,12 @@ FRAME_TIME = "frameTime"
 
 
 
-def set_labels(utterance_filenames, input_dir, frame_size, frame_step):
+def perform_frame_labeling(utterance_filenames,
+                            input_dir,
+                            frame_size,
+                            frame_step):
     """
-        Takes a chunk of filenames (one per each utterance) containing features.
+        Takes a chunk of filenames (one file per each utterance) containing features.
         A labeled set of utterances (frame by frame) will be returned, specifying
         if that frame is part of a vowel or not.
     """
@@ -53,7 +70,7 @@ def set_labels(utterance_filenames, input_dir, frame_size, frame_step):
     labeled_utterances = []
 
     for feature_file, i in zip(utterance_filenames, np.arange(len(utterance_filenames))):
-        #if feature_file.endswith("30.csv"):
+        # if feature_file.endswith("30.csv"):
         # Load utterance frames with features
         expr_features = pd.read_csv(input_dir + CSV_PATH + feature_file,
                                     delimiter = ";")
@@ -73,7 +90,6 @@ def set_labels(utterance_filenames, input_dir, frame_size, frame_step):
 
         # Convert sample number to time (seconds)
         expr_phones.loc[:, "start":"end"] *= SAMPLE_FREQUENCY
-        # expr_time_length = expr_phones.iloc[-1, expr_phones.columns.get_loc("end")]
 
         # Extract start points of each frame
         frame_start_points = np.asarray(expr_features.loc[:, FRAME_TIME])
@@ -95,18 +111,22 @@ def set_labels(utterance_filenames, input_dir, frame_size, frame_step):
                     nextPhonePart = (frame_ending -
                                         expr_phones.loc[phone, "end"]) / frame_size
 
-                    if (phone + 1 < tot_phones \
-                        and nextPhonePart >= ADEGUACY):
-                        # more than 1-tollerance * 100 % of this frame is on the next phone
+                    if phone + 1 < tot_phones and nextPhonePart >= ADEGUACY:
+                        # more than ADEGUACY*100% of this frame covers the next phone
                         labels.append(int(expr_phones.loc[phone + 1, "type"] in vowels))
                     else:
-                        # less than 1-tollerance * 100 % of this frame is on the next phone
+                        # less than ADEGUACY*100% of this frame covers the next phone
                         labels.append(int(expr_phones.loc[phone, "type"] in vowels))
 
 
         expr_label_data = pd.DataFrame(labels, index = None, columns = ["vowel"])
         labeled_utterance = pd.concat([expr_features, expr_label_data], axis = 1)
 
+        # Throwing away frame start info which is no more relevant
+        labeled_utterance.drop(labeled_utterance.columns[0], axis = 1, inplace = True)
+
+        # In the final .csv utterances will be separated by a blank line,
+        # so at the end of utterance's DataFrame a vector of NaNs must be inserted
         final_features = list(labeled_utterance.columns.values)
         nans = np.full([len(final_features)], np.nan)
         s = pd.Series(nans, index = final_features)
@@ -118,7 +138,7 @@ def set_labels(utterance_filenames, input_dir, frame_size, frame_step):
 
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     """
         The main purpose of this parallel labeling is to optimize time, so we don't
         care about memory usage here.
@@ -127,34 +147,28 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory",
                         help = "Path containing wav, phn and csv directories.")
-    parser.add_argument("-v", "--validationset",
-                        help = "Transform a slice of dataset into validationset, \
-                        with propoprions that are specified in " + PART_PATH + ". \
-                        \nNote that the rest of the dataset content will be created \
-                        as trainset.",
-                        action = "store_true")
+    parser.add_argument("-o", "--output",
+                        help = "Name of the output file containing labeled data")
+    parser.add_argument("-c", "--cores", type = int,
+                        help = "Number of physical core to use")
     args = parser.parse_args()
 
-    if args.directory:
+    if args.directory and args.output:
+        phys_cores = PHYS_CORES
+        if args.cores:
+            phys_cores = args.cores
+
         input_dir = args.directory
+        output_file = args.output
+
+        if not output_file.endswith(CSV_EXT):
+            output_file += CSV_EXT
+
         feature_files = os.listdir(input_dir + CSV_PATH)
         tot_files = len(feature_files)
 
-        sets = []
-        if args.validationset:
-            train_files, val_files, w, v = train_test_split(feature_files,
-                                                np.zeros(len(feature_files)),
-                                                test_size = 0.2,
-                                                train_size = 0.8)
-
-            # Sets must be appended in this order: train, validation, test
-            sets.append(train_files)
-            sets.append(val_files)
-            del w, v
-        else:
-            sets.append(feature_files)
-
-
+        # Searching for frameSize and frameStep infos inside specified
+        # openSMILE configuration file
         featureConfig = cp.ConfigParser(comment_prefixes = ("/", ";", "#"),
                                         strict = True)
         featureConfig.read(CONFIG_PATH)
@@ -168,42 +182,42 @@ if __name__=="__main__":
         print ("\nDetected frame size:", frame_size, "s")
         print ("Detected frame step:", frame_step, "s")
 
-
         vowels = np.asarray(pd.read_csv(VOWELS, header = None), dtype = str)
         vowels = np.reshape(vowels, len(vowels))
         print ("\n", len(vowels),"vowel symbols: ", *vowels, sep = " ")
-    else:
-        print ("No input directory provided.")
 
-    for set_type, set_name in zip(sets, SETS):
-        chunks = np.array_split(set_type, PROC)
+        # Slicing data for some parallel processing
+        chunks = np.array_split(feature_files, phys_cores)
 
-        pool = Pool(processes = PROC)
+        pool = Pool(processes = phys_cores)
 
         start_time = timeit.default_timer()
-        labeled_utterances = pool.starmap(set_labels, zip(chunks,
+        labeled_utterances = pool.starmap(perform_frame_labeling, zip(chunks,
                                                     it.repeat(input_dir),
                                                     it.repeat(frame_size),
                                                     it.repeat(frame_step)))
         elapsed_time = timeit.default_timer() - start_time
-        print (set_name, elapsed_time)
+        print (output_file, elapsed_time)
 
         # Remove .csv from previous computations, if exists
-        os.remove(OUTPUT + set_name + CSV_EXT)
+        if os.path.exists(OUTPUT + output_file):
+            os.remove(OUTPUT + output_file)
 
         features = list(labeled_utterances[0][0].columns.values)
         for utterances in labeled_utterances:
             for utterance in utterances:
-                if not os.path.isfile(OUTPUT + set_name + CSV_EXT):
+                if not os.path.isfile(OUTPUT + output_file):
                     # if file does not exist write header
-                    utterance.to_csv(OUTPUT + set_name + CSV_EXT,
+                    utterance.to_csv(OUTPUT + output_file,
                                             header = features,
                                             index = False,
                                             sep = ";")
                 else:
                     # if it exists then simply append
-                    utterance.to_csv(OUTPUT + set_name + CSV_EXT,
+                    utterance.to_csv(OUTPUT + output_file,
                                             mode = "a",
                                             header = False,
                                             index = False,
                                             sep = ";")
+    else:
+        parser.print_help()
